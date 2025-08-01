b1742e4feebebd35423b3ca044af5e83df77c3e4
Orbzilla <52539671+Orbzilla@users.noreply.github.com>
2025-08-01 14:48:58 -0400

    Update addThought.js

--- api/addThought.js
+++ api/addThought.js
@@ -23,8 +23,8 @@ export default async function handler(req, res) {
         },
         body: JSON.stringify({
           fields: {
-  "Thought": Thought,
-  "Location": Location,
+                "Thought": thought,
+                "Location": location,
           },
         }),
       }
