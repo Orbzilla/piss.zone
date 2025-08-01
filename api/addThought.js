db81c737d7bc4d9b125c108b0426ebb3bb810c2f
Orbzilla <52539671+Orbzilla@users.noreply.github.com>
2025-08-01 10:28:25 -0400

    Update addThought.js

--- api/addThought.js
+++ api/addThought.js
@@ -1,6 +1,6 @@
-res.setHeader("Access-Control-Allow-Origin", "*");
 
 export default async function handler(req, res) {
+
   if (req.method !== "POST") {
     return res.status(405).send("Method Not Allowed");
   }
