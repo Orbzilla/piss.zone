42d2c6963a8f34af9d40913909af31c1a0505fcf
Orbzilla <52539671+Orbzilla@users.noreply.github.com>
2025-08-01 10:23:50 -0400

    Update addThought.js

--- api/addThought.js
+++ api/addThought.js
@@ -6,6 +6,8 @@ export default async function handler(req, res) {
   }
 
   const { thought, location } = req.body;
+  console.log("Incoming thought:", thought);
+  console.log("Incoming location:", location);
 
   try {
     const airtableRes = await fetch(`https://api.airtable.com/v0/${process.env.AIRTABLE_BASE_ID}/Thoughts`, {
@@ -23,8 +25,10 @@ export default async function handler(req, res) {
     });
 
     const data = await airtableRes.json();
+    console.log("AIRTABLE RESPONSE:", data); // 👈 This is what we want to see
     res.status(200).json(data);
   } catch (err) {
+    console.error("ERROR:", err);
     res.status(500).json({ error: err.message });
   }
 }
