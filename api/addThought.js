a96fd5df14b34360944fdaf27b42214a7634c167
Orbzilla <52539671+Orbzilla@users.noreply.github.com>
2025-08-01 15:07:42 -0400

    Update addThought.js

--- api/addThought.js
+++ api/addThought.js
@@ -1,8 +1,8 @@
-export default async function handler(req, res) {
+
+async function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method Not Allowed" });
   }
-
   const BASE_ID = process.env.AIRTABLE_BASE_ID;
   const TABLE_NAME = "Messages";
   const API_KEY = process.env.AIRTABLE_API_KEY;
